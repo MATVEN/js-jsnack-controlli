41a48cb09f2cbf777fdddc3f69262cbe3c656aec
MATVEN <mattiavena2993@gmail.com>
2019-10-28 09:41:09 +0100

    edit error

--- dispari/js/main.js
+++ dispari/js/main.js
@@ -1,13 +1,16 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 //un numero, se è dispari inseriscilo nell’array.
 
-var numero = [];
+var dispari = [];
 
 // ciclo
 for (var i = 0; i < 6; i++) {
   var numero = parseInt(prompt('Scegli un numero'));
 
-// cndition
-if (i % 2 != 0) {
-  console.log(number);
+
+// condition
+  if (numero % 2 != 0) {
+    dispari.push(numero);
+    console.log(numero);
+  }
 }
