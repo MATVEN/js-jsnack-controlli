2ffde2aa41326d631c2380b1652af8fe58d001cf
MATVEN <mattiavena2993@gmail.com>
2019-10-28 09:19:51 +0100

    ex 5

--- dispari/js/main.js
+++ dispari/js/main.js
@@ -1,2 +1,13 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 //un numero, se è dispari inseriscilo nell’array.
+
+var numero = [];
+
+// ciclo
+for (var i = 0; i < 6; i++) {
+  var numero = parseInt(prompt('Scegli un numero'));
+
+// cndition
+if (i % 2 != 0) {
+  console.log(number);
+}
